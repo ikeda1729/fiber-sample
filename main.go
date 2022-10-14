40373d47397b23e908f7052f2f07f07efbd9c5c3
ikeda1729 <d.enkikairo@gmail.com>
2022-10-14 23:58:20 +0900

    adds AllowOrigins

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ func main() {
 	app.Use(cors.New(cors.Config{
 		// 認証にcookieなどの情報を必要とするかどうか
 		AllowCredentials: true,
+		AllowOrigins:     os.Getenv("FRONTEND_URL"),
 	}))
 
 	database.ConnectDB()
