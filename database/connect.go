fc5b012f4ffadc57eb6c051e03d8f1e23a29b408
ikeda1729 <d.enkikairo@gmail.com>
2022-10-14 21:56:02 +0900

    uses os.Getenv for heroku

--- database/connect.go
+++ database/connect.go
@@ -1,9 +1,10 @@
 package database
 
 import (
-	"api-fiber-gorm/config"
 	"api-fiber-gorm/model"
+	"api-fiber-gorm/seed"
 	"fmt"
+	"os"
 	"strconv"
 
 	"gorm.io/driver/postgres"
@@ -14,9 +15,9 @@ import (
 // ConnectDB connect to db
 func ConnectDB() {
 	var err error
-	p := config.Config("DB_PORT")
+	p := os.Getenv("DB_PORT")
 	port, err := strconv.ParseUint(p, 10, 32)
-	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=require", config.Config("DB_HOST"), port, config.Config("DB_USER"), config.Config("DB_PASSWORD"), config.Config("DB_NAME"))
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=require", os.Getenv("DB_HOST"), port, os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{Logger: logger.Default.LogMode(logger.Info)})
 
 	if err != nil {
@@ -27,24 +28,24 @@ func ConnectDB() {
 	// DB.Migrator().DropTable(&model.Tweet{}, &model.User{}, "user_followees")
 	DB.AutoMigrate(&model.Tweet{}, &model.User{})
 	fmt.Println("Database Migrated")
-	// if err := seed.UserSeed(DB, "./seed/users.csv"); err != nil {
-	// 	fmt.Println(err)
-	// }
-	// if err := seed.TweetSeed(DB, "./seed/tweets.csv"); err != nil {
-	// 	fmt.Println(err)
-	// }
-	// if err := seed.FolloweesSeed(DB, "./seed/user_followees.csv"); err != nil {
-	// 	fmt.Println(err)
-	// }
-	// fmt.Println("Seeds added")
+	if err := seed.UserSeed(DB, "./seed/users.csv"); err != nil {
+		fmt.Println(err)
+	}
+	if err := seed.TweetSeed(DB, "./seed/tweets.csv"); err != nil {
+		fmt.Println(err)
+	}
+	if err := seed.FolloweesSeed(DB, "./seed/user_followees.csv"); err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Seeds added")
 }
 
 // ConnectDB connect to db
 func InitTestDB() {
 	var err error
-	p := config.Config("DB_PORT")
+	p := os.Getenv("DB_PORT")
 	port, err := strconv.ParseUint(p, 10, 32)
-	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", config.Config("DB_HOST"), port, config.Config("DB_USER"), config.Config("DB_PASSWORD"), config.Config("DB_NAME"))
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", os.Getenv("DB_HOST"), port, os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
