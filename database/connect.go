237c0bcb40f2b4a3c9f821a86bd53e13fc862739
ikeda1729 <d.enkikairo@gmail.com>
2022-10-14 21:58:46 +0900

    print debug

--- database/connect.go
+++ database/connect.go
@@ -2,7 +2,6 @@ package database
 
 import (
 	"api-fiber-gorm/model"
-	"api-fiber-gorm/seed"
 	"fmt"
 	"os"
 	"strconv"
@@ -19,6 +18,7 @@ func ConnectDB() {
 	port, err := strconv.ParseUint(p, 10, 32)
 	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=require", os.Getenv("DB_HOST"), port, os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{Logger: logger.Default.LogMode(logger.Info)})
+	print("dsnForDebug: ", dsn)
 
 	if err != nil {
 		panic("failed to connect database")
@@ -28,16 +28,16 @@ func ConnectDB() {
 	// DB.Migrator().DropTable(&model.Tweet{}, &model.User{}, "user_followees")
 	DB.AutoMigrate(&model.Tweet{}, &model.User{})
 	fmt.Println("Database Migrated")
-	if err := seed.UserSeed(DB, "./seed/users.csv"); err != nil {
-		fmt.Println(err)
-	}
-	if err := seed.TweetSeed(DB, "./seed/tweets.csv"); err != nil {
-		fmt.Println(err)
-	}
-	if err := seed.FolloweesSeed(DB, "./seed/user_followees.csv"); err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println("Seeds added")
+	// if err := seed.UserSeed(DB, "./seed/users.csv"); err != nil {
+	// 	fmt.Println(err)
+	// }
+	// if err := seed.TweetSeed(DB, "./seed/tweets.csv"); err != nil {
+	// 	fmt.Println(err)
+	// }
+	// if err := seed.FolloweesSeed(DB, "./seed/user_followees.csv"); err != nil {
+	// 	fmt.Println(err)
+	// }
+	// fmt.Println("Seeds added")
 }
 
 // ConnectDB connect to db
